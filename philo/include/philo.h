30047c4bbfae80de1a1d5b926677147f126790da
Daniela <daniloustalot95@gmail.com>
2025-07-04 17:01:00 +0200

    Added Makefile

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   philo.c                                            :+:    :+:            */
+/*   philo.h                                            :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: dloustal <marvin@42.fr>                      +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2025/07/04 16:19:42 by dloustalot    #+#    #+#                 */
-/*   Updated: 2025/07/04 16:59:28 by dloustalot    ########   odam.nl         */
+/*   Created: 2025/07/04 16:50:26 by dloustalot    #+#    #+#                 */
+/*   Updated: 2025/07/04 17:00:16 by dloustalot    ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
-
-int	main(int argc, char **argv)
-{
-	(void)argv;
-	if (argc < 5 || argc > 6)
-	{
-		printf("Usage: <number_of_philos> <time_to_die> <time_to_eat> <time_to_sleep> [times_each_philo_must_eat]\n");
-		return (1);
-	}
-	return (0);
-}
+#include <pthread.h>
+#include <stdio.h>
