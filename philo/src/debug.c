4cf25e4cc4fcc386e9bb9cee71230625a2c70215
Daniela Loustalot Knapp <dloustal@student.codam.nl>
2025-07-08 15:58:25 +0200

    Experimenting with fsanitize

--- philo/src/debug.c
+++ philo/src/debug.c
@@ -6,7 +6,7 @@
 /*   By: dloustal <dloustal@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/07 15:32:16 by dloustal      #+#    #+#                 */
-/*   Updated: 2025/07/07 15:58:09 by dloustal      ########   odam.nl         */
+/*   Updated: 2025/07/08 15:47:37 by dloustal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ void	print_philos(t_philo **philos, t_param *params)
 	i = 0;
 	while (i < params->num_philos)
 	{
-		printf("Philo #%d:\nTimes eaten: %d\nThread id: %ld\n\n",
-			philos[i]->index, philos[i]->times_eaten, philos[i]->tid);
+		printf("Philo #%d:\nTimes eaten: %d\nThread id: %ld\n\n", philos[i]->index, philos[i]->times_eaten, philos[i]->tid);
 		i++;
 	}
 }
