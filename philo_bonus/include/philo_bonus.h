203cee07f0f91a3469b8e157036581c35c237926
Daniela Loustalot Knapp <dloustal@student.codam.nl>
2025-07-21 16:48:06 +0200

    Started bonus

---
+++ philo_bonus/include/philo_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philo_bonus.h                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dloustal <dloustal@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/07/21 15:20:19 by dloustal      #+#    #+#                 */
+/*   Updated: 2025/07/21 15:40:30 by dloustal      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+#define PHILO_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+# define YELLOW "\e[1;33m"
+# define YELlOW_B "\e[1;43m"
+# define PURPLE "\e[1;35m"
+# define PURPLE_B "\e[1;45m"
+# define RED "\e[1;31m"
+# define BLUE "\e[1;34m"
+# define GRAY "\e[1;37m"
+# define GREEN "\e[1;32m"
+# define RESET "\e[0m"
+
+#endif
---
+++ philo_bonus/src/atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   atoi.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dloustal <dloustal@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/07/17 19:00:42 by dloustal      #+#    #+#                 */
+/*   Updated: 2025/07/17 19:01:01 by dloustal      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include <stdbool.h>
+
+static bool	is_space(char c)
+{
+	return (c == ' ' || c == '\f' || c == '\n'
+		|| c == '\r' || c == '\t' || c == '\v');
+}
+
+static bool	is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+static bool	is_sign(char c)
+{
+	return (c == '+' || c == '-');
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int		nb;
+	int		sign;
+	char	*str;
+
+	str = (char *)nptr;
+	nb = 0;
+	sign = 1;
+	while (is_space(*str))
+		str++;
+	if (is_sign(*str))
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (is_digit(*str))
+	{
+		nb *= 10;
+		nb += *str - '0';
+		str++;
+	}
+	return (nb * sign);
+}
